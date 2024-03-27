cbe427a5d9edcaf218a605c77012f50ab9f28a11
alexmcmahonsoftware <alexmcmahon960505@gmail.com>
2024-03-27 14:19:28 +0000

    attribute name fixed

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import Game from './components/Game';
 
 function App() {
   return (
-    <div class="App">
+    <div className="App">
       <Game />
     </div>
   );
--- app/src/components/GameNavigator.js
+++ app/src/components/GameNavigator.js
@@ -3,15 +3,15 @@ import calculateWinner from './utils.js';
 
 function GameNavigator({move, historyLen, cells, navigateHandler}) {
 	const prevButton = (
-		<button class={"navigator" + (move > 0 ? "" : " disabled")} onClick={() => navigateHandler(Math.max(0, move - 1))}>{"<"}</button>
+		<button className={"navigator" + (move > 0 ? "" : " disabled")} onClick={() => navigateHandler(Math.max(0, move - 1))}>{"<"}</button>
 	);
 	const winner = calculateWinner(cells);
 	const statusText = winner == null ? (move % 2 ? "✘" : "✔") : ("⚑" + (move % 2 ? "✔" : "✘") + "⚑");
 	const statusCell = (
-		<button class={"navigator " + (winner == null ? "on-progress" : "game-over")} onClick={() => navigateHandler(0)}>{statusText}</button>
+		<button className={"navigator " + (winner == null ? "on-progress" : "game-over")} onClick={() => navigateHandler(0)}>{statusText}</button>
 	);
 	const nextButton = (
-		<button class={"navigator" + (move + 1 < historyLen ? "" : " disabled")} onClick={() => navigateHandler(Math.min(move + 1, historyLen - 1))}>{">"}</button>
+		<button className={"navigator" + (move + 1 < historyLen ? "" : " disabled")} onClick={() => navigateHandler(Math.min(move + 1, historyLen - 1))}>{">"}</button>
 	);
 	return (
 		<table>
