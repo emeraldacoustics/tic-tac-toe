45fb221b43ef0dbb45af2d1b76c120aa66e432aa
alexmcmahonsoftware <alexmcmahon960505@gmail.com>
2024-03-27 02:37:16 +0000

    navigation modified

--- app/src/components/GameNavigator.js
+++ app/src/components/GameNavigator.js
@@ -3,12 +3,12 @@ import calculateWinner from './utils.js';
 
 function GameNavigator({move, historyLen, cells, navigateHandler}) {
 	const prevButton = (
-		<button class="navigator" onClick={() => navigateHandler(Math.max(0, move - 1))}>{"<"}</button>
+		<button class={"navigator" + (move > 0 ? "" : " disabled")} onClick={() => navigateHandler(Math.max(0, move - 1))}>{"<"}</button>
 	);
 	const winner = calculateWinner(cells);
 	const statusText = winner == null ? (move % 2 ? "✘" : "✔") : ("⚑" + (move % 2 ? "✔" : "✘") + "⚑");
 	const statusCell = (
-		<button class={"navigator " + (winner == null ? "on-progress" : "game-over")}>{statusText}</button>
+		<button class={"navigator " + (winner == null ? "on-progress" : "game-over")} onClick={() => navigateHandler(0)}>{statusText}</button>
 	);
 	const nextButton = (
 		<button class={"navigator" + (move + 1 < historyLen ? "" : " disabled")} onClick={() => navigateHandler(Math.min(move + 1, historyLen - 1))}>{">"}</button>
